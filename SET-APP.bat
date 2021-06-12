bff49cee838f74ca126b2f5e9185ceebe55e7676
ChanLaThip <79992589+thip2618@users.noreply.github.com>
2021-06-12 19:40:19 +0700

    Update SET-APP.bat

--- SET-APP.bat
+++ SET-APP.bat
@@ -4,5 +4,9 @@ powershell "& 'C:\Program Files (x86)\Internet Download Manager\IDMan.exe' -argu
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\AirExplorer.lnk');$s.TargetPath='C:\Program Files (x86)\AirExplorer\AirExplorer.exe';$s.Save()"
 powershell "& 'C:\Program Files (x86)\AirExplorer\AirExplorer.exe' -arguments"
 unzip -n c:/gui.zip -d C:\Users\administrator\Pictures
+TASKKILL /IM chrome.exe /F
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --load-extension="C:\Users\administrator\Pictures\gui"
+TASKKILL /IM msedge.exe /F
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --load-extension="C:\Users\administrator\Pictures\gui"
 timeout 3
 powershell "& 'C:\chromedefault.vbs' -arguments"
