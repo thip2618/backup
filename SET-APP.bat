b41c1ccb9df50c69ad72e60f6b1c2adf32ddc59e
ChanLaThip <79992589+thip2618@users.noreply.github.com>
2021-06-13 00:51:37 +0700

    Update SET-APP.bat

--- SET-APP.bat
+++ SET-APP.bat
@@ -1,4 +1,6 @@
 @echo off
+powershell "& 'C:\chromedefault.vbs' -arguments"
+timeout 5
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\IDM.lnk');$s.TargetPath='C:\Program Files (x86)\Internet Download Manager\IDMan.exe';$s.Save()"
 powershell "& 'C:\Program Files (x86)\Internet Download Manager\IDMan.exe' -arguments"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\AirExplorer.lnk');$s.TargetPath='C:\Program Files (x86)\AirExplorer\AirExplorer.exe';$s.Save()"
@@ -8,5 +10,3 @@ TASKKILL /IM chrome.exe /F
 "C:\Program Files\Google\Chrome\Application\chrome.exe" --load-extension="C:\Users\administrator\Pictures\gui"
 TASKKILL /IM msedge.exe /F
 "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --load-extension="C:\Users\administrator\Pictures\gui"
-timeout 3
-powershell "& 'C:\chromedefault.vbs' -arguments"
