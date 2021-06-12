d99816b899343b76bb4a4369a90ebd9abd0deefc
ChanLaThip <79992589+thip2618@users.noreply.github.com>
2021-06-12 16:27:38 +0700

    Update SET-APP.bat

--- SET-APP.bat
+++ SET-APP.bat
@@ -1,4 +1,3 @@
 @echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './caiidm.cmd'" > out.txt 2>&1
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\IDM.lnk');$s.TargetPath='C:\Program Files (x86)\Internet Download Manager\IDMan.exe';$s.Save()"
 powershell "& 'C:\Program Files (x86)\Internet Download Manager\IDMan.exe' -arguments"
