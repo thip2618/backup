022362d167f91e07553338407fde24ed2984045e
ChanLaThip <79992589+thip2618@users.noreply.github.com>
2021-06-12 19:02:58 +0700

    Update SET-APP.bat

--- SET-APP.bat
+++ SET-APP.bat
@@ -3,6 +3,6 @@ powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Des
 powershell "& 'C:\Program Files (x86)\Internet Download Manager\IDMan.exe' -arguments"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\AirExplorer.lnk');$s.TargetPath='C:\Program Files (x86)\AirExplorer\AirExplorer.exe';$s.Save()"
 powershell "& 'C:\Program Files (x86)\AirExplorer\AirExplorer.exe' -arguments"
-unzip c:/gui.zip -d C:\Users\administrator\Pictures
+unzip -n c:/gui.zip -d C:\Users\administrator\Pictures
 timeout 3
 powershell "& 'C:\chromedefault.vbs' -arguments"
