929dbf47f11436f1ea207221b16c75e589bc2208
ChanLaThip <79992589+thip2618@users.noreply.github.com>
2021-06-13 17:12:32 +0700

    Update SET-APP.bat

--- SET-APP.bat
+++ SET-APP.bat
@@ -1,6 +1,8 @@
 @echo off
 powershell "& 'C:\chromedefault.vbs' -arguments"
 timeout 5
+powershell "& 'C:\chromedefault.vbs' -arguments"
+timeout 5
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\IDM.lnk');$s.TargetPath='C:\Program Files (x86)\Internet Download Manager\IDMan.exe';$s.Save()"
 powershell "& 'C:\Program Files (x86)\Internet Download Manager\IDMan.exe' -arguments"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\AirExplorer.lnk');$s.TargetPath='C:\Program Files (x86)\AirExplorer\AirExplorer.exe';$s.Save()"
