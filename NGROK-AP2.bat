5548b2b2a6655e3710b97ccb5984dd48e2d4f27d
ChanLaThip <79992589+thip2618@users.noreply.github.com>
2024-02-28 17:31:30 +0700

    Update NGROK-AP2.bat

--- NGROK-AP2.bat
+++ NGROK-AP2.bat
@@ -1,1 +1,1 @@
-echo region: us >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
+echo region: ap >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
