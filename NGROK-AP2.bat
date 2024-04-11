c7d386f6be1894278614741b064cc6927a5dc7bd
ChanLaThip <79992589+thip2618@users.noreply.github.com>
2024-04-11 08:41:28 +0700

    Update NGROK-AP2.bat

--- NGROK-AP2.bat
+++ NGROK-AP2.bat
@@ -1,2 +1,2 @@
-echo region: us >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
-nssm install ngrok .\ngrok.exe start --all --config=".\ngrok.yml"
+echo region: ap >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
+nssm install ngrok ngrok.exe start --all --config=".\ngrok.yml"
